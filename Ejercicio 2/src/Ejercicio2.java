03cd8fb1a06e9d5a4d9adb9c2c915c97f702a6fa
Nuriarueda <ruedareinanuria@gmail.com>
2024-02-07 10:56:57 +0100

    capitulo 11

---
+++ Ejercicio 2/src/Ejercicio2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        
+        String line = "";
+        
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("primos.dat"));
+            
+            do{
+                line = br.readLine();
+                System.out.println(line != null ? line : "");
+            }while(line != null);
+
+            br.close();
+
+        }catch(IOException io){
+            System.out.println("No se ha encontrado el archivo");
+        }
+    }
+}
