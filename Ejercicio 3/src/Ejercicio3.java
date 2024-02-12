6e82c1434e35eb66d1e9ab000b96376e0d58b4ce
Nuriarueda <ruedareinanuria@gmail.com>
2024-02-12 18:16:39 +0100

    capitulo 11

--- Ejercicio 3/src/Ejercicio3.java
+++ Ejercicio 3/src/Ejercicio3.java
@@ -15,12 +15,13 @@ public class   Ejercicio3 {
         try {
             BufferedReader bf1 = new BufferedReader(new FileReader(args[0]));
             BufferedReader bf2 = new BufferedReader(new FileReader(args[1]));
-            BufferedReader bw = new BufferedReader(new FileReader(args[2]));
+            BufferedWriter bw = new BufferedWriter(new FileWriter(args[2]));
 
             String linea1 = "";
             String linea2 = "";
 
-            while ((linea1 != null ) || (linea2 != null)) {
+            while ( (linea1 != null) || (linea2 != null) ) {
+
                 linea1 = bf1.readLine();
                 linea2 = bf2.readLine();
 
@@ -31,8 +32,8 @@ public class   Ejercicio3 {
                 if (linea2 != null) {
                     bw.write(linea2 + "\n");
                 }
-                
             }
+            
             bf1.close();
             bf2.close();
             bw.close();
