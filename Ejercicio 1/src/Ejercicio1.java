dff396c34113f9bbd06c6dda9a98022cb2d9947e
Nuriarueda <ruedareinanuria@gmail.com>
2024-02-12 15:12:51 +0100

    capitulo 11

--- Ejercicio 1/src/Ejercicio1.java
+++ Ejercicio 1/src/Ejercicio1.java
@@ -1,3 +1,6 @@
+/* Escribe un programa que guarde en un fichero con nombre primos.dat los
+números primos que hay entre 1 y 500. */
+
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
