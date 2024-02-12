5312bb25842c9542ca85006b5315117050e25b9e
Nuriarueda <ruedareinanuria@gmail.com>
2024-02-12 15:36:53 +0100

    capitulo 11

--- Ejercicio 1/src/Ejercicio1.java
+++ Ejercicio 1/src/Ejercicio1.java
@@ -7,48 +7,30 @@ import java.io.IOException;
 
 public class Ejercicio1{
     public static void main(String[] args) {
-        
-        
-        int nPrimo = 1;
-        String cadena = "";
-        
-        try{
 
+        try {
             BufferedWriter bw = new BufferedWriter(new FileWriter("primos.dat"));
-            String text = nPrimo + ", \n";
-            
-            do{    
-                nPrimo = siguientePrimo(nPrimo);
-                cadena = (nPrimo==499 ? nPrimo + "\n": nPrimo + ", \n");
-                bw.write(cadena);
-            }while(nPrimo<499);
 
-            bw.close();
-            
-        }catch(IOException e){
-            System.out.println("No se ha podido escribir en el fichero");
-        }
-
-
-    }
-
-    public static boolean esPrimo(int n){
-
-            for(int i = 2; i < n ;  i++){
-                if(n%i == 0){
-                    return false;
+            for (int i = 1; i < 501; i++){
+                if(esPrimo(i)){
+                    bw.write(String.valueOf(i) + "\n");
                 }
             }
 
-            return true;
+            bw.close();
+        } catch (IOException ioe) {
+            System.out.println("Error");
+        }
     }
 
-    public static int siguientePrimo(int n){
+    public static boolean esPrimo(int x){
 
-        do{
-            n++;
-        }while(!esPrimo(n));
-
-        return n;
+        for (int i = 2; i < x; i++){
+            if ((x%i) == 0) {
+                return false;
+            }
+        }
+        return true;
     }
+        
 }
